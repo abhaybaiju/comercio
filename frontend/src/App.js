bfb17f019219054e808e18f755a70d147497b479
Abhay Baiju <abhay0198@gmail.com>
2020-09-10 14:11:42 +0530

    Setup basic flask and react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState("Hello");
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, [currentTime]);
+
+  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  d.setUTCSeconds(currentTime);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +29,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {d.toTimeString()}.</p>
       </header>
     </div>
   );
