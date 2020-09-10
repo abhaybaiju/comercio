cdf0ece4e6bb231d78ebf79746c64159c821f47a
Abhay Baiju <abhay0198@gmail.com>
2020-09-10 19:02:42 +0530

    Created barebones Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Dashboard from './components/Dashboard';
 function App() {
 
-  const [currentTime, setCurrentTime] = useState("Hello");
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, [currentTime]);
-
-  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-  d.setUTCSeconds(currentTime);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {d.toTimeString()}.</p>
-      </header>
+      <Dashboard/>
     </div>
   );
 }
