4b71ceaeb5662144d493a1811d3f0d0f86e376bb
shubham-saudolla <shubhamsaudolla@gmail.com>
2020-09-11 12:59:53 +0530

    Printing the orders to console

--- backend/randomOrderGenerator.py
+++ backend/randomOrderGenerator.py
@@ -1,12 +1,15 @@
-from datetime import datetime
 import random
-from securityGenerator import equity, equityNames, equityList, GetEquityAttributes, UpdateBatchSize, GetRandomPrice
+from datetime import datetime
+
+from backend.securityGenerator import equityNames, UpdateBatchSize, GetRandomPrice
+
 # default ISIN index
 index = 100000000
 indexLocked = bool(0)
 
 buyOrdersList = list()
 sellOrdersList = list()
+
 
 # the price doubles up as the market/limit order indicator
 # if the price is 0, the order is a market order, else it is a limit order
@@ -21,7 +24,13 @@ class order:
         self.price = int(price)
         self.stamp = str(datetime.now())
 
-def RandomGenerator(self, minOrders = 50 , maxOrders = 100):
+    def RetrieveOrderAttributes(self):
+        resultList = list()
+        resultList.extend([self.id, self.name, self.quantity, self.allOrNothing, self.buyOrSell, self.price, self.stamp])
+        return resultList
+
+
+def RandomGenerator(self, minOrders=50, maxOrders=100):
     if minOrders > maxOrders:
         t = minOrders
         minOrders = maxOrders
@@ -34,25 +43,42 @@ def RandomGenerator(self, minOrders = 50 , maxOrders = 100):
     indexLocked = 1
 
     for i in range(minOrders, maxOrders):
-        index+=1
+        index += 1
         orderID = index
         orderName = equityNames[random.randint(0, (len(equityNames) - 1))]
         orderQuantity = random.randint(1, 200)
         orderAON = bool(random.getrandbits(1))
         orderBOS = bool(random.getrandbits(1))
-        if(random.getrandbits(1)):
+        if (random.getrandbits(1)):
             orderPrice = GetRandomPrice(orderName)
         else:
             orderPrice = 0
 
-        if(orderBOS == 0):
+        if (orderBOS == 0):
             buyOrdersList.append(order(orderID, orderName, orderQuantity, orderAON, orderBOS, orderPrice))
         else:
             sellOrdersList.append(order(orderID, orderName, orderQuantity, orderAON, orderBOS, orderPrice))
         UpdateBatchSize(orderName)
+
 
 RandomGenerator(100, 200)
 print(len(buyOrdersList))
 print(len(sellOrdersList))
 
 
+def PrintOrderDetails(x):
+    global buyOrdersList, sellOrdersList
+
+    if x == 0:
+        print('Printing the buy orders:')
+        for i in buyOrdersList:
+            result = i.RetrieveOrderAttributes()
+            print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]) + ' '+ str(result[3]) + ' '+ str(result[4]) + ' '+ str(result[5]) + ' '+ str(result[6]))
+    else:
+        print('Printing the sell orders:')
+        for i in sellOrdersList:
+            result = i.RetrieveOrderAttributes()
+            print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]) + ' '+ str(result[3]) + ' '+ str(result[4]) + ' '+ str(result[5]) + ' '+ str(result[6]))
+
+PrintOrderDetails(0)
+PrintOrderDetails(1)
