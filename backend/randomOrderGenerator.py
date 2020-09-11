b67ee55ea41a279dd14a2ed37a10a26e3578e22f
shubham-saudolla <shubhamsaudolla@gmail.com>
2020-09-11 13:19:21 +0530

    Added comments, bug fix

--- backend/randomOrderGenerator.py
+++ backend/randomOrderGenerator.py
@@ -4,9 +4,12 @@ from datetime import datetime
 from backend.securityGenerator import equityNames, UpdateBatchSize, GetRandomPrice
 
 # default ISIN index
+# for each new day the order would be X00000000 where X is the number of the day
 index = 100000000
+# variable that locks off any changes to the index during random order generation
 indexLocked = bool(0)
 
+# separate lists that contain order objects
 buyOrdersList = list()
 sellOrdersList = list()
 
@@ -24,12 +27,15 @@ class order:
         self.price = int(price)
         self.stamp = str(datetime.now())
 
+    # returns the atttributes of the order as a list
     def RetrieveOrderAttributes(self):
         resultList = list()
-        resultList.extend([self.id, self.name, self.quantity, self.allOrNothing, self.buyOrSell, self.price, self.stamp])
+        resultList.extend(
+            [self.id, self.name, self.quantity, self.allOrNothing, self.buyOrSell, self.price, self.stamp])
         return resultList
 
 
+# the random order generator, default arguments are
 def RandomGenerator(self, minOrders=50, maxOrders=100):
     if minOrders > maxOrders:
         t = minOrders
@@ -39,10 +45,14 @@ def RandomGenerator(self, minOrders=50, maxOrders=100):
         minOrders = 50
         maxOrders = 100
 
+    numberOfOrders = random.randint(minOrders, maxOrders)
+
     global index, indexLocked, buyOrdersList, sellOrdersList
+
+    # locking the index
     indexLocked = 1
 
-    for i in range(minOrders, maxOrders):
+    for i in range(0, numberOfOrders):
         index += 1
         orderID = index
         orderName = equityNames[random.randint(0, (len(equityNames) - 1))]
@@ -60,12 +70,16 @@ def RandomGenerator(self, minOrders=50, maxOrders=100):
             sellOrdersList.append(order(orderID, orderName, orderQuantity, orderAON, orderBOS, orderPrice))
         UpdateBatchSize(orderName)
 
+    indexLocked = 0
+
 
 RandomGenerator(100, 200)
 print(len(buyOrdersList))
 print(len(sellOrdersList))
 
 
+# prints the order details from the two order list
+# argument 0 is for the buy list and 1 for the sell list
 def PrintOrderDetails(x):
     global buyOrdersList, sellOrdersList
 
@@ -73,12 +87,15 @@ def PrintOrderDetails(x):
         print('Printing the buy orders:')
         for i in buyOrdersList:
             result = i.RetrieveOrderAttributes()
-            print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]) + ' '+ str(result[3]) + ' '+ str(result[4]) + ' '+ str(result[5]) + ' '+ str(result[6]))
+            print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]) + ' ' + str(result[3]) + ' ' + str(
+                result[4]) + ' ' + str(result[5]) + ' ' + str(result[6]))
     else:
         print('Printing the sell orders:')
         for i in sellOrdersList:
             result = i.RetrieveOrderAttributes()
-            print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]) + ' '+ str(result[3]) + ' '+ str(result[4]) + ' '+ str(result[5]) + ' '+ str(result[6]))
+            print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]) + ' ' + str(result[3]) + ' ' + str(
+                result[4]) + ' ' + str(result[5]) + ' ' + str(result[6]))
+
 
 PrintOrderDetails(0)
 PrintOrderDetails(1)
