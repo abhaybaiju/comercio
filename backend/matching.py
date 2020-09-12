caba71017d3e95a0f4f4f4420285ece836f660bf
shubham-saudolla <shubhamsaudolla@gmail.com>
2020-09-12 13:29:53 +0530

    dirty commit

---
+++ backend/matching.py
@@ -0,0 +1,63 @@
+import random
+from datetime import datetime
+
+import pandas as pd
+
+from backend.securityGenerator import equityNames, UpdateBatchSize, GetRandomPrice
+from backend.randomOrderGenerator import index, indexLocked, RandomGenerator, PrintOrderDetails, RetrieveSorted
+
+# ordersDf = RandomGenerator(100, 200)
+
+def OrderMatching():
+    ordersDf = RandomGenerator(100, 200)
+    # print('look at this')
+    # print(ordersDf[ordersDf.LOM == 'l'])
+    # print('*****')
+    for srno, i in ordersDf.iterrows():
+        print(len(ordersDf.index))
+        # ordersDf = ordersDf.drop((ordersDf.id == str(i.iloc[0])).index, inplace=True)
+        ordersDf = ordersDf[ordersDf.id != str(i.iloc[0])]
+        print(len(ordersDf.index))
+        # if (i.iloc[4] == 's' and i.iloc[5] == 'l'):
+        #     for sr, j in ordersDf.loc[(ordersDf[4].isin(['b']))].iterrows():
+        #         if j.iloc[6] >= i.iloc[6] and i.iloc[1] == j.iloc[1]:
+        #             if i.iloc[2] == j.iloc[2]:
+        #                 ordersDf = ordersDf.drop(ordersDf.id == [i.iloc[0], j.iloc[0]].index, inplace=True)
+        #                 # indexNames = dfObj[dfObj['Age'] == 30].index
+        #                 # print('trade executed')
+        #                 # print('buy order:')
+        #                 # print(j)
+        #                 # print('sell order:')
+        #                 # print(i)
+        # if (i.iloc[4] == 's' and i.iloc[5] == 'm'):
+        #     tempDf = RetrieveSorted(0)
+        #     for sr, j in tempDf.iterrows():
+        #             if i.iloc[2] == j.iloc[2]:
+        #                 ordersDf = ordersDf.drop(ordersDf.id == [i.iloc[0], j.iloc[0]].index, inplace=True)
+        #                 # print('trade executed')
+        #                 # print('buy order:')
+        #                 # print(i)
+        #                 # print('sell order:')
+        #                 # print(j)
+        # if i.iloc[4] == 'b' and i.iloc[5] == 'l':
+        #     for sr, j in ordersDf.loc[(ordersDf[4].isin(['s']))].iterrows():
+        #         if j.iloc[6] <= i.iloc[6]:
+        #             if i.iloc[2] == j.iloc[2]:
+        #                 ordersDf = ordersDf.drop(ordersDf.id == [i.iloc[0], j.iloc[0]].index, inplace=True)
+        #                 # print('trade executed')
+        #                 # print('buy order:')
+        #                 # print(j)
+        #                 # print('sell order:')
+        #                 # print(i)
+        # if i.iloc[4] == 'b' and i.iloc[5] == 'm':
+        #     tempDf = RetrieveSorted(1)
+        #     for sr, j in tempDf.iterrows():
+        #         if j.iloc[6] <= i.iloc[6]:
+        #             if i.iloc[2] == j.iloc[2]:
+        #                 ordersDf = ordersDf.drop(ordersDf.id == [i.iloc[0], j.iloc[0]].index, inplace=True)
+        #                 # print('trade executed')
+        #                 # print('buy order:')
+        #                 # print(i)
+        #                 # print('sell order:')
+        #                 # print(j)
+    # print(ordersDf)
