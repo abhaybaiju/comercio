e99cd8f015c4ebf648bec342f56cd489b4856f3a
TejasPathak98 <tejaspathak98@gmail.com>
2020-09-13 02:22:14 -0700

    changes in server.py

--- backend/server.py
+++ backend/server.py
@@ -5,58 +5,161 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'server.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mrig.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+class Securities(db.Model):
+    security_name = db.Column(db.String(80), primary_key=True)
+    isin = db.Column(db.String(80), nullable=False)
+    type = db.Column(db.String(80), nullable=False)
+    ltprice = db.Column(db.Integer)
+    Orders = db.relationship('Order', backref='owner', lazy='dynamic')
+    Rejected_Orders = db.relationship('Rejected_Order', backref='owner', lazy='dynamic')
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+
+def __init__(self, security_name, isin, type, ltprice):
+    self.security_name = security_name
+    self.isin = isin
+    self.type = type
+    self.ltprice = ltprice
 
 
-class UserSchema(ma.Schema):
+class SecuritiesSchema(ma.Schema):
     class Meta:
-        # Fields to expose
-        fields = ('username', 'email')
+        fields = ('security_name', 'isin', 'type', 'ltprice')
 
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+Security_Schema = SecuritiesSchema()
+Securities_Schema = SecuritiesSchema(many=True)
 
 
-# endpoint to create new user
-@app.route("/user", methods=["POST"])
-def add_user():
-    username = request.json['username']
-    email = request.json['email']
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    price = db.Column(db.Integer, nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    aon = db.Column(db.String(80), nullable=False)
+    identifier = db.Column(db.Integer, nullable=False, default=0)
+    BOS = db.Column(db.String(80), nullable=False, default='b')
+    LOM = db.Column(db.String(80), nullable=False)
+    Order_isin = db.Column(db.String(80), db.ForeignKey('securities.isin'))
+
+    def __init__(self,Order_isin, price, qty, aon, identifier, BOS, LOM):
+        self.Order_isin = Order_isin
+        self.price = price
+        self.qty = qty
+        self.aon = aon
+        self.identifier = identifier
+        self.BOS = BOS
+        self.LOM = LOM
+
+
+class OrderSchema(ma.Schema):
+    class Meta:
+        fields = ('Order_isin', 'price', 'qty', 'aon', 'identifier', 'BOS', 'LOM')
+
+
+Order_Schema = OrderSchema()
+Orders_Schema = OrderSchema(many=True)
+
+
+class Rejected_Order(db.Model):
+    sr_no = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    price = db.Column(db.Integer, nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    aon = db.Column(db.String(80), nullable=False)
+    LOM = db.Column(db.String(80), nullable=False)
+    BOS = db.Column(db.String(80), nullable=False)
+    Order_isin = db.Column(db.String(80), db.ForeignKey('securities.isin'))
+
+    def __init__(self,Order_isin, price, qty, aon, BOS, LOM):
+        self.Order_isin = Order_isin
+        self.price = price
+        self.qty = qty
+        self.aon = aon
+        self.BOS = BOS
+        self.LOM = LOM
+
     
-    new_user = User(username, email)
+class RejectedOrderSchema(ma.Schema):
+    class Meta:
+        fields = ('Order_isin', 'price', 'qty', 'aon', 'BOS', 'LOM')
 
-    db.session.add(new_user)
+
+RejectedOrder_Schema = RejectedOrderSchema()
+RejectedOrders_Schema = RejectedOrderSchema(many=True)
+
+
+# endpoint to add a new security
+@app.route("/security", methods=["POST"])
+def add_security():
+    security_name = request.json['security_name']
+    isin = request.json['isin']
+    type = request.json['type']
+    ltprice = request.json['ltprice']
+
+    new_security = Securities(security_name = security_name , isin = isin , type = type , ltprice = ltprice)
+    db.session.add(new_security)
     db.session.commit()
+    return Security_Schema.jsonify(new_security)
 
-    return user_schema.jsonify(new_user)
 
-
-# endpoint to show all users
-@app.route("/user", methods=["GET"])
-def get_user():
-    all_users = User.query.all()
-    result = users_schema.dump(all_users)
+# endpoint to show all securities
+@app.route("/securities", methods=["GET"])
+def get_securities():
+    all_securities = Securities.query.all()
+    result = Securities_Schema.dump(all_securities)
     return jsonify(result)
 
 
-# endpoint to get user detail by id
-@app.route("/user/<id>", methods=["GET"])
-def user_detail(id):
-    user = User.query.get(id)
-    return user_schema.jsonify(user)
+# endpoint to add a new Order
+@app.route("/order", methods=["POST"])
+def add_order():
+    Order_isin = request.json['Order_isin']
+    price = request.json['price']
+    qty = request.json['qty']
+    aon = request.json['aon']
+    identifier = request.json['identifier']
+    BOS = request.json['BOS']
+    LOM = request.json['LOM']
+
+    new_order = Order(Order_isin, price, qty, aon, identifier, BOS, LOM)
+    db.session.add(new_order)
+    db.session.commit()
+    return Order_Schema.jsonify(new_order)
+
+
+# endpoint to show all orders
+@app.route("/orders", methods=["GET"])
+def get_orders():
+    all_orders = Order.query.all()
+    result = Orders_Schema.dump(all_orders)
+    return jsonify(result)
+
+
+# endpoint to add a new Rejected Order
+@app.route("/Rejectedorder", methods=["POST"])
+def add_Rejected_order():
+    Order_isin = request.json['Order_isin']
+    price = request.json['price']
+    qty = request.json['qty']
+    aon = request.json['aon']
+    BOS = request.json['BOS']
+    LOM = request.json['LOM']
+
+    new_Rejected_order = Rejected_Order(Order_isin,price,qty,aon,BOS,LOM)
+    db.session.add(new_Rejected_order)
+    db.session.commit()
+    return Order_Schema.jsonify(new_Rejected_order)
+
+
+# endpoint to show all Rejected Orders
+@app.route("/Rejectedorders", methods=["GET"])
+def get_Rejected_orders():
+    all_Rejected_orders = Order.query.all()
+    result = Orders_Schema.dump(all_Rejected_orders)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
