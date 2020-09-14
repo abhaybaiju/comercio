08115b639923d3a09e3ce78a9d26adafd0b63bbb
shubham-saudolla <shubhamsaudolla@gmail.com>
2020-09-14 14:35:37 +0530

    Code cleanup

--- backend/matcher.py
+++ backend/matcher.py
@@ -26,17 +26,17 @@ def Match():
             # s l
             if i[4] == 's' and i[5] == 'l':
                 for j in ordersList:
-                        if j[4] == 'b' and marker[ReturnJC(ordersList, j[0])] == 0:
-                            if j[6] >= i[6] and i[1] == j[1]:
-                                if i[2] == j[2]:
-                                    marker[iC] += 1
-                                    doneOrders.append([ordersList[iC]])
-                                    jC = ReturnJC(ordersList, j[0])
-                                    marker[jC] += 1
-                                    doneOrders.append(ordersList[jC])
-                                    doneOrders.append(['***'])
-                                    count = count + 1
-                                    break
+                    if j[4] == 'b' and marker[ReturnJC(ordersList, j[0])] == 0:
+                        if j[6] >= i[6] and i[1] == j[1]:
+                            if i[2] == j[2]:
+                                marker[iC] += 1
+                                doneOrders.append([ordersList[iC]])
+                                jC = ReturnJC(ordersList, j[0])
+                                marker[jC] += 1
+                                doneOrders.append(ordersList[jC])
+                                doneOrders.append(['***'])
+                                count = count + 1
+                                break
             # b l
             elif i[4] == 'b' and i[5] == 'l':
                 for j in ordersList:
@@ -57,7 +57,7 @@ def Match():
                 for j in tempList:
                     jC = ReturnJC(ordersList, j[0])
                     if marker[jC] == 0 and i[2] == j[2] and i[1] == j[1] and j[4] == 'b':
-                        marker[iC]+=1
+                        marker[iC] += 1
                         doneOrders.append([ordersList[iC]])
                         marker[jC] += 1
                         doneOrders.append(ordersList[jC])
@@ -69,14 +69,14 @@ def Match():
                 for j in tempList:
                     jC = ReturnJC(ordersList, j[0])
                     if marker[jC] == 0 and i[2] == j[2] and i[1] == j[1] and j[4] == 's':
-                        marker[iC]+=1
+                        marker[iC] += 1
                         doneOrders.append([ordersList[iC]])
                         marker[jC] += 1
                         doneOrders.append(ordersList[jC])
                         doneOrders.append(['***'])
                         count = count + 1
                         break
-            # # b m
+            # b m
         iC = iC + 1
 
     for i in marker:
@@ -88,6 +88,7 @@ def Match():
         if i == 1:
             flagged = flagged + 1
     print(str(flagged) + ' were found')
+
 
 def SortList(inputList, indexOfList, reverseOrNot):
     res = sorted(inputList, key=itemgetter(indexOfList), reverse=reverseOrNot)
@@ -102,9 +103,9 @@ def ReturnJC(inputList, jID):
         else:
             counter = counter + 1
 
+
 def Delete(iC, jC):
     global ordersList
 
 
 Match()
-
