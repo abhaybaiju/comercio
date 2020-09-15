eb29e0ee6fe30bd6cf2abea21afdc22372ee8753
shubham-saudolla <shubhamsaudolla@gmail.com>
2020-09-15 10:49:56 +0530

    Inserts into securities table

--- backend/orderDB.py
+++ backend/orderDB.py
@@ -1,19 +1,3 @@
 import sqlite3
-from sqlite3 import Error
 
-conn = None
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    global conn
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-
-if __name__ == '__main__':
-    create_connection('omsdb.db')
-
+conn = sqlite3.connect('omsdb.db')
--- backend/securityGenerator.py
+++ backend/securityGenerator.py
@@ -1,17 +1,18 @@
 import math
 import random
-
+from backend.orderDB import conn
 equityNames = list(('Apple', 'Microsoft', 'IBM', 'Xerox', 'Pixar'))
 
 
 # the equities class
 class equity:
     # constructor for the equity objects
-    def __init__(self, name, price, batchSize, trend):
+    def __init__(self, name, price, batchSize, trend, isin):
         self.name = name
         self.price = price
         self.batchSize = batchSize
         self.trend = bool(trend)
+        self.isin = isin
 
     def PrintAtrributes(self):
         print(self.name + ' ' + self.price + ' ' + self.batchSize + ' ' + self.trend)
@@ -31,14 +32,25 @@ class equity:
             self.trend = random.getrandbits(1)
 
 
+equityCursor = conn.cursor()
+
 # creating the list of the equity objects
 equityList = list()
-equityList.append(equity('Apple', 700, 5, 1))
-equityList.append(equity('Microsoft', 500, 4, 1))
-equityList.append(equity('IBM', 350, 5, 0))
-equityList.append(equity('Xerox', 200, 3, 0))
-equityList.append(equity('Pixar', 550, 7, 1))
-
+equityList.append(equity('Apple', 700, 5, 1, 'APPLE1984'))
+equityCursor.execute('''INSERT INTO Securities_Index values(?, ?, ?, ?);''', ('APPLE1984', 'Apple', 'Stock', 700))
+conn.commit()
+equityList.append(equity('Microsoft', 500, 4, 1, 'MIC1990'))
+equityCursor.execute('''INSERT INTO Securities_Index values(?, ?, ?, ?);''', ('MIC1990', 'Microsoft', 'Stock', 500))
+conn.commit()
+equityList.append(equity('IBM', 350, 5, 0, 'IBM1950'))
+equityCursor.execute('''INSERT INTO Securities_Index values(?, ?, ?, ?);''', ('IBM1950', 'IBM', 'Stock', 350))
+conn.commit()
+equityList.append(equity('Xerox', 200, 3, 0, 'XER1960'))
+equityCursor.execute('''INSERT INTO Securities_Index values(?, ?, ?, ?);''', ('XER1960', 'Xerox', 'Stock', 200))
+conn.commit()
+equityList.append(equity('Pixar', 550, 7, 1, 'PIX1991'))
+equityCursor.execute('''INSERT INTO Securities_Index values(?, ?, ?, ?);''', ('PIX1991', 'Pixar', 'Stock', 550))
+conn.commit()
 
 # function to retrieve the attributes of a particular equity
 def GetEquityAttributes(find):
