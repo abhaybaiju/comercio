75ebb10580dfa6b27962fe266bd2b1faeaafda44
shubham-saudolla <shubhamsaudolla@gmail.com>
2020-09-18 00:41:17 +0530

    Updated new server

--- backend/new_server.py
+++ backend/new_server.py
@@ -3,41 +3,51 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import jsonify
 from flask import flash, request
-from flaskext.mysql import MySQL
+import mysql.connector
+from threading import Thread
+import os
+from backend.matcher import Match
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="oms"
+)
+
+
+class MyFlaskApp(Flask):
+    def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+        if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+            with self.app_context():
+                Thread(target=Match).start()
+        super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
 
 app = Flask(__name__)
 CORS(app)
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
-app.config['MYSQL_DATABASE_DB'] = 'oms'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
 
 
 # endpoint to show all securities
 @app.route('/securities', methods=['GET'])
 def show_security():
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        global conn
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM Securities_Index;")
         empRows = cursor.fetchall()
         response = jsonify(empRows)
         response.status_code = 200
         return response
-
     except Exception as e:
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to add  securities
 @app.route('/addsecurity', methods=['POST'])
 def add_security():
+    global conn
     try:
         _json = request.json
         _ISIN = _json['ISIN']
@@ -48,7 +58,6 @@ def add_security():
         if _ISIN and _name and _type and _ltprice and request.method == 'POST':
             sqlQuery = "INSERT INTO securities_index(ISIN,name,type,ltprice) VALUES(%s, %s, %s, %s);"
             bindData = (_ISIN, _name, _type, _ltprice)
-            conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
             conn.commit()
@@ -57,20 +66,18 @@ def add_security():
             return response
         else:
             return not_found()
-
     except Exception as e:
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to show all orders
 @app.route('/orders', methods=['GET'])
 def show_orders():
+    global conn
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM Order_Index;")
         empRows = cursor.fetchall()
         response = jsonify(empRows)
@@ -81,12 +88,12 @@ def show_orders():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to add orders
 @app.route('/addorder', methods=['POST'])
 def add_order():
+    global conn
     try:
         _json = request.json
         _id = _json['id']
@@ -101,7 +108,6 @@ def add_order():
         if _ISIN and _id and _price and _qty and _identifier and _BOS and _LOM and request.method == 'POST':
             sqlQuery = "INSERT INTO order_index(id,ISIN,price,qty,aon,identifier,BOS,LOM) VALUES(%s, %s, %s, %s ,%s, %s, %s ,%s);"
             bindData = (_id, _ISIN, _price, _qty,_aon,_identifier, _BOS, _LOM)
-            conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
             conn.commit()
@@ -110,36 +116,33 @@ def add_order():
             return response
         else:
             return not_found()
-
     except Exception as e:
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to show all Rejected orders
 @app.route('/Rejectedorders', methods=['GET'])
 def show_Rejectedorders():
+    global conn
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM Rejected_Order;")
         empRows = cursor.fetchall()
         response = jsonify(empRows)
         response.status_code = 200
         return response
-
     except Exception as e:
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to add Rejected orders
 @app.route('/addRejectedorder', methods=['POST'])
 def add_Rejectedorder():
+    global conn
     try:
         _json = request.json
         _sr_no = _json['sr_no']
@@ -153,7 +156,6 @@ def add_Rejectedorder():
         if _sr_no and _ISIN and _price and _BOS and _qty and _aon and _LOM and request.method == 'POST':
             sqlQuery = "INSERT INTO rejected_order(sr_no,ISIN,price,BOS,qty,aon,LOM) VALUES(%s, %s, %s, %s, %s, %s, %s);"
             bindData = (_sr_no,_ISIN, _price, _BOS, _qty, _aon, _LOM)
-            conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
             conn.commit()
@@ -167,15 +169,14 @@ def add_Rejectedorder():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to show all Manual orders
 @app.route('/Manualorders', methods=['GET'])
 def show_Manualorders():
+    global conn
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM  Manual_Orders;")
         empRows = cursor.fetchall()
         response = jsonify(empRows)
@@ -186,12 +187,12 @@ def show_Manualorders():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to add Rejected orders
 @app.route('/addManualorder', methods=['POST'])
 def add_Manualorder():
+    global conn
     try:
         _json = request.json
         _ISIN = _json['ISIN']
@@ -205,7 +206,6 @@ def add_Manualorder():
         if _ISIN and _price and _qty and _aon and _identifier and _BOS and _LOM and request.method == 'POST':
             sqlQuery = "INSERT INTO manual_orders(ISIN,price,qty,aon,identifier,BOS,LOM) VALUES(%s, %s, %s, %s,%s,%s,%s);"
             bindData = (_ISIN, _price, _qty, _aon, _identifier, _BOS, _LOM)
-            conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
             conn.commit()
@@ -219,15 +219,14 @@ def add_Manualorder():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to show all Trades
 @app.route('/trade', methods=['GET'])
 def show_trade():
+    global conn
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM  Trade_Index;")
         empRows = cursor.fetchall()
         response = jsonify(empRows)
@@ -238,12 +237,12 @@ def show_trade():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to add trade
 @app.route('/addtrade', methods=['POST'])
 def add_trade():
+    global conn
     try:
         _json = request.json
         _id = _json['id']
@@ -254,7 +253,6 @@ def add_trade():
         if _id and _buyorder_id and _sellorder_id and _price and _qty and request.method == 'POST':
             sqlQuery = "INSERT INTO trade_index(id,buyorder_id,sellorder_id,price,qty) VALUES(%s,%s, %s, %s, %s);"
             bindData = (_id, _buyorder_id, _sellorder_id, _price, _qty)
-            conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
             conn.commit()
@@ -268,15 +266,14 @@ def add_trade():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to show portfolio
 @app.route('/portfolio', methods=['GET'])
 def show_porfolio():
+    global conn
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM  My_Portfolio;")
         empRows = cursor.fetchall()
         response = jsonify(empRows)
@@ -287,12 +284,12 @@ def show_porfolio():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 # endpoint to add to portfolio
 @app.route('/addportfolio', methods=['POST'])
 def add_portfolio():
+    global conn
     try:
         _json = request.json
         _ISIN = _json['ISIN']
@@ -302,7 +299,6 @@ def add_portfolio():
         if _ISIN and _name and _qty and request.method == 'POST':
             sqlQuery = "INSERT INTO my_portfolio(ISIN,name,qty) VALUES(%s,%s, %s);"
             bindData = (_ISIN, _name, _qty)
-            conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sqlQuery, bindData)
             conn.commit()
@@ -316,7 +312,6 @@ def add_portfolio():
         print(e)
     finally:
         cursor.close()
-        conn.close()
 
 
 @app.errorhandler(404)
